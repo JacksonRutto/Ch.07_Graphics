043f2b39391925c1eab0f0dcb1008523ade9efa9
JacksonRutto <ruttojac@urbandaleschools.com>
2019-04-11 12:24:47 -0500

    starting flag

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,19 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+
+import arcade
+
+arcade.open_window(500, 500, "Star Wars Art")
+arcade.set_background_color(arcade.color.WHITE)
+
+arcade.start_render()
+'''
+
+'''
+arcade.finish_render()
+
+arcade.run()
+
+
+
