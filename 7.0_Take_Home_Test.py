6e02d65501178fea76286c228bd025929d669081
JacksonRutto <ruttojac@urbandaleschools.com>
2019-04-26 12:18:04 -0500

    working on ch. 7 take home test

--- 7.0_Take_Home_Test.py
+++ 7.0_Take_Home_Test.py
@@ -10,7 +10,15 @@ The picture is 500px wide and 400px tall. Look up ARC in the documentation to do
 
 import arcade
 
-arcade.open_window(500, 500, "Take Home Test")
-arcade.set_background_color(arcade.color.WHITE)
+arcade.open_window(500, 400, "Take Home Test")
+arcade.set_background_color(arcade.color.ALMOND)
 
 arcade.start_render()
+
+for x_offset in range(0, 400, 20):
+    arcade.draw_line(0, 0+x_offset, 500, 0+x_offset, arcade.color.BLACK, 1)
+
+
+arcade.finish_render()
+
+arcade.run()
