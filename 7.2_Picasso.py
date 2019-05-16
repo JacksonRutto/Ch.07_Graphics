ef8e301140258b39000e90f9c8d12d4ba26c54a8
JacksonRutto <ruttojac@urbandaleschools.com>
2019-05-16 12:25:20 -0500

    finished picasso project

--- 7.2_Picasso.py
+++ 7.2_Picasso.py
@@ -16,6 +16,38 @@ When you are finished Pull Request your file to your instructor.
 '''
 import arcade
 
-arcade.open_window(500, 500, "Picasso Project")
+arcade.open_window(500, 400, "Picasso Project")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
 
+arcade.draw_rectangle_filled(250, 200, 300, 250, arcade.color.BROWN_NOSE) #head
+
+arcade.draw_circle_filled(200, 250, 30, arcade.color.BLACK) #eyes
+arcade.draw_circle_filled(300, 250, 30, arcade.color.WHITE)
+arcade.draw_circle_filled(200, 250, 10, arcade.color.PALATINATE_BLUE) #pupils
+arcade.draw_circle_filled(300, 250, 10, arcade.color.ORCHID_PINK)
+
+
+arcade.draw_arc_filled(250, 175, 50, 25, arcade.color.ORANGE, 90, 180) #beak/nose
+
+arcade.draw_arc_filled(250, 145, 75, 45, arcade.color.YELLOW, 180, 360) #mouth
+
+arcade.draw_text("What's up!?", 195, 40, arcade.color.BLUE_YONDER, 20)
+
+for x_offset in range(5, 302, 15):      #hair
+    arcade.draw_line(102+x_offset, 300, 102+x_offset, 335, arcade.color.PAKISTAN_GREEN, 4)
+
+
+
+
+
+
+
+
+
+
+
+
+arcade.finish_render()
+arcade.run()
 
